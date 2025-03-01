46cf07bae5fc7a96b00f5237656a76514b51294b
Davi Henrique <henridavhi26@gmail.com>
2025-03-01 11:03:58 -0300

    refactor: desativado o CORS e renomeado o arquivo de banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 // ----------------------------------------------------------------
 
-const dbPath = path.resolve(__dirname, 'database.sqlite');
+const dbPath = path.resolve(__dirname, 'dbLojas.sqlite');
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
